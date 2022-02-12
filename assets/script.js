3479ed8d4355da73e14b49d5a910620505003677
Dany Dias <diasdany2708@gmail.com>
2022-02-12 10:00:06 +0100

    restructure code html

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,29 @@
 const navigation = document.querySelector('.scrollDownFooter');
-window.addEventListener('scroll', () => {
+const navigationScroll = document.querySelector('#header-menu');
+const modalContainer2 = document.querySelector(".modal-container2");
+const modalTriggers2 = document.querySelectorAll(".modal-trigger2");
+const modalContainer = document.querySelector(".modal-container");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
 
+
+window.addEventListener('scroll', () => {
     if(window.scrollY > 10){
         navigation.classList.add('anim-scrollDownFooter');
-    } else {
-        navigation.classList.remove('anim-scrollDownFooter');
-    }
-})
-
-
-
-const navigationScroll = document.querySelector('#header-menu');
-window.addEventListener('scroll', () => {
-
-    if(window.scrollY > 10){
         navigationScroll.classList.add('anim-header-menu');
     } else {
+        navigation.classList.remove('anim-scrollDownFooter');
         navigationScroll.classList.remove('anim-header-menu');
     }
 })
 
 
-
-const modalContainer = document.querySelector(".modal-container");
-const modalTriggers = document.querySelectorAll(".modal-trigger");
-
 modalTriggers.forEach(trigger => trigger.addEventListener("click", toggleModal))
-
 function toggleModal(){
   modalContainer.classList.toggle("active")
 }
 
 
-
-const modalContainer2 = document.querySelector(".modal-container2");
-const modalTriggers2 = document.querySelectorAll(".modal-trigger2");
-
 modalTriggers2.forEach(trigger => trigger.addEventListener("click", toggleModalBis))
-
 function toggleModalBis(){
   modalContainer2.classList.toggle("active")
 }
